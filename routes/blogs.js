b2db0a630aa514e25f346e232f77a3b3d009309b
aaronlacey <aaronlacey98@gmail.com>
2023-02-22 15:13:09 -0800

    final update

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,136 +2,102 @@ const { v4: uuidv4 } = require("uuid");
 const express = require('express');
 const router = express.Router();
 
-//instantiate mongodb 
+//-----------------Initialize Mongo----------------//
 const { db } = require('../mongo');
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   const blogs = await db()
-  .collection('sample_blogs')
-  .find({})
-  .limit(5)
-  .toArray(function(err, result){
+    .collection('sample_blogs')
+    .find({})
+    .limit(5)
+    .toArray(function (err, result) {
       if (err) {
         res.status(400).send("error fetching blogs")
       } else {
         res.json(result);
       }
-    }); 
-
-    res.json({
-      success:true,
-      blogs: blogs
     });
-    
+
+  res.json({
+    success: true,
+    blogs: blogs
+  });
+
 });
 
-//GET all blogs from database
-router.get('/all', async function(req, res) {
-    const blogs = await db()
+// -------------Return all blogs from database----------------//
+
+router.get('/all', async function (req, res) {
+  const blogs = await db()
     .collection('sample_blogs')
     .find({})
     .limit(5)
-    .toArray(function(err, result){
-        if (err) {
-          res.status(400).send("error fetching blogs")
-        } else {
-          res.json(result);
-        }
-      }); 
-      res.json({
-        success:true,
-        blogs: blogs
-      });
-      
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("error fetching blogs")
+      } else {
+        res.json(result);
+      }
+    });
+  res.json({
+    success: true,
+    blogs: blogs
   });
 
-//GET one blog from database
-  router.get('/get-one', async function(req, res) {
-    const blogs = await db()
+});
+
+//-----------------Return one blog from database----------------//
+router.get('/get-one', async function (req, res) {
+  const blogs = await db()
     .collection('sample_blogs')
     .find({})
     .limit(1)
-    .toArray(function(err, result){
-        if (err) {
-          res.status(400).send("error fetching blogs")
-        } else {
-          res.json(result);
-        }
-      }); 
-  
-      res.json({
-        success:true,
-        blogs: blogs
-      });
-      
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("error fetching blogs")
+      } else {
+        res.json(result);
+      }
+    });
+
+  res.json({
+    success: true,
+    blogs: blogs
   });
 
-//GET one blog from id search
-  router.get('/get-one/:idToFind', async function(req, res) {
-    const blogs = await db()
+});
+
+//-------------------Return one blog from database by id----------------//
+router.get('/get-one/:idToFind', async function (req, res) {
+  const blogs = await db()
     .collection('sample_blogs')
     .findOne({
-        id:req.params.id
+      id: req.params.id
     })
-      res.json({
-        success:true,
-        blogs: blogs
-      });
+  res.json({
+    success: true,
+    blogs: blogs
   });
+});
 
-//POST new blog
-  router.post('/create-one', async function(req, res, next) {
-    const blogs = await db()
+//-------------------Creates one blog in database----------------//
+router.post('/create-one', async function (req, res, next) {
+  const blogs = await db()
     .collection('sample_blogs')
-    .insertOne({    
-        title: req.body.title,
-        text: req.body.text,
-        author: req.body.author,
-        category: req.body.category,
-        last_modified: new Date(),
-        })
+    .insertOne({
+      title: req.body.title,
+      text: req.body.text,
+      author: req.body.author,
+      category: req.body.category,
+      last_modified: new Date(),
+    })
 
-      res.json({
-        success:true,
-        blogs: blogs
-      });
+  res.json({
+    success: true,
+    blogs: blogs
   });
-
-//  STRETCH GOALS :
-//get-multi/ (GET): get multiple sorted results
-//HINT: use find().sort() 
-router.get('/get-multi', async function(req, res, next) {
-  // console.log(req.params.opt1)
-  const blogs = await db()
-  .collection('sample_blogs')
-  .find({title: { $in: [req.query.opt1,req.query.opt2,req.query.opt3]}})
-  .sort(
-    {title: 1, blogs: 1}
-  )
-  .toArray()
-
-    res.json({
-      success:true,
-      blogs: blogs
-    });
-})
+});
 
 
-
-//delete-multi/ (GET): get multiple results
-//HINT: use deleteMany()
-
-router.get('/get-one/:titleToFind', async function(req, res) {
-  const blogs = await db()
-  .collection('sample_blogs')
-  .findOne({
-      title:req.params.titleToFind
-  })
-    res.json({
-      success:true,
-      blogs: blogs
-    });
-})
-  
-module.exports = router;
+//-------------------End Of Code----------------//
